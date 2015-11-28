9f84baa8a361871a6da73e2ecc246249e1f6d038
unknown <>
2015-11-28 22:38:42 +0800

    基本界面的实现

---
+++ app/src/main/java/com/example/administrator/ustc_health/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.ustc_health;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by HBL on 2015/11/28.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
