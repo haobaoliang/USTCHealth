b9485927662f7faa3749a4c2afdc37abc6c98d0d
unknown <>
2015-12-03 21:53:00 +0800

    第二三个注册界面

--- app/src/main/java/com/example/administrator/ustc_health/RegisterActivityPh.java
+++ app/src/main/java/com/example/administrator/ustc_health/RegisterActivityPh.java
@@ -1,27 +1,25 @@
 package com.example.administrator.ustc_health;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.internal.widget.ButtonBarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
 /**
- * Created by HBL on 2015/12/2.
+ * Created by HBL on 2015/12/3.
  */
-public class RegisterActivity extends AppCompatActivity {
-
+public class RegisterActivityPh extends AppCompatActivity {
     private Button nextbutton;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reg);
-        nextbutton=(Button)findViewById(R.id.reg_button);
+        setContentView(R.layout.activity_reg_ph);
+        nextbutton=(Button)findViewById(R.id.reg_ph_button);
 
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.reg_toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.reg_ph_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Sign Up");
@@ -29,7 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent();
-                intent.setClass(RegisterActivity.this,RegisterActivityPh.class);
+                intent.setClass(RegisterActivityPh.this,RegisterActivityFin.class);
                 startActivity(intent);
             }
         });
